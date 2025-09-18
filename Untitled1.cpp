19602b74464c3dd077fe82ca57b215cb1f182d09
Trần Tùng <huan05092007@gmail.com>
2025-09-18 19:38:41 +0700

    init

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*6.kiem tra chan le.*/
+	int n;
+	cout<<"Nhap mot so: ";
+	cin>>n;
+	if(n%2==0)
+	    cout<<n<<" la so chan!";
+	else
+	    cout<<n<<" la so le!";
+	    
+	return 0;    
+	    
+}
